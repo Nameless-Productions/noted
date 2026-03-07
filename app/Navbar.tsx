db3082480ed66b3b6336771ec6cc02d49388490e
Nameless <nameless@thenamelessdev.com>
2026-03-07 23:06:43 +0100

    Made navbar: array of pages, title

---
+++ app/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react'
+
+export default function Navbar() {
+    const pages = [{title: "Home", href: "/"}, {title: "Todos", href: "/todos"}, {title: "Notes", href: "/notes"}];
+
+  return (
+    <nav className='flex border-b-2 border-black space-x-3 p-2 bg-gray-100 items-center text-black'>
+        <Link href="/" className='font-bold text-2xl transition-all duration-300 hover:font-normal'>Noted</Link>
+        {pages.map((page) => (
+            <Link className='text-lg text-black hover:font-bold transition-all duration-200' href={page.href}>{page.title}</Link>
+        ))}
+    </nav>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './globals.css';
+import Navbar from './Navbar';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang='en'>
       <body>
+        <Navbar />
         <main className='m-3'>
           {children}
         </main>

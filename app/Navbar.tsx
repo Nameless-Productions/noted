8091e36513139627ae983a6c5d7f993f865d844b
Nameless <nameless@thenamelessdev.com>
2026-03-08 09:06:12 +0100

    fix "key" error in navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <nav className='flex border-b-2 border-black space-x-3 p-2 bg-gray-100 items-center text-black'>
         <Link href="/" className='font-bold text-2xl transition-all duration-300 hover:font-normal'>Noted</Link>
         {pages.map((page) => (
-            <Link className='text-lg text-black hover:font-bold transition-all duration-200' href={page.href}>{page.title}</Link>
+            <Link className='text-lg text-black hover:font-bold transition-all duration-200' href={page.href} key={page.href}>{page.title}</Link>
         ))}
     </nav>
   )

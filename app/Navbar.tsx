d8ab4a7f3b9a95412f332d5a6863537f389adaf3
thenamelessdev <nameless@thenamelessdev.com>
2026-03-09 15:06:08 +0100

    add profile to navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -10,6 +10,18 @@ export default function Navbar() {
         {pages.map((page) => (
             <Link className='text-lg text-black hover:font-bold transition-all duration-200' href={page.href} key={page.href}>{page.title}</Link>
         ))}
+
+        <details className='relative ml-auto'>
+          <summary className='text-lg text-black hover:font-bold transition-all duration-200 cursor-pointer list-none [&::-webkit-details-marker]:hidden'>
+            Profile
+          </summary>
+
+          <div className='absolute right-0 mt-2 w-44 rounded-md border border-gray-300 bg-white shadow-md z-50'>
+            <Link href="/me" className='block px-4 py-2 hover:bg-gray-100'>My profile</Link>
+            <Link href="/settings" className='block px-4 py-2 hover:bg-gray-100'>Settings</Link>
+            <Link href="/logout" className='block px-4 py-2 hover:bg-gray-100'>Log out</Link>
+          </div>
+        </details>
     </nav>
   )
 }
